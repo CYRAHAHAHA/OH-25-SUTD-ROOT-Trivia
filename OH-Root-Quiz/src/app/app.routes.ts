7587bf72c4aa7dbca33c02f68f2d66c9d7adee22
CYRAHAHAHA <92076623+CYRAHAHAHA@users.noreply.github.com>
2024-12-25 19:02:21 +0800

    Auto-generated commit

--- OH-Root-Quiz/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { QuizPageComponent } from './quiz-page/quiz-page.component';
-import { ResultsPageComponent } from './results-page/results-page.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/quiz', pathMatch: 'full' },
-  { path: 'quiz', component: QuizPageComponent },
-  { path: 'results', component: ResultsPageComponent }
-];
