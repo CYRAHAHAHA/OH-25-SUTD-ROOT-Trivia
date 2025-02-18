eb46b01b676e502d9b09c8ee2456de0e0d647750
CYRAHAHAHA <92076623+CYRAHAHAHA@users.noreply.github.com>
2025-02-18 18:58:08 +0100

    reduce paddings, now work on smaller screens

--- OH-Root-Quiz/tailwind.config.js
+++ OH-Root-Quiz/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': {'max': '400px'},
+      },
+    },
   },
   plugins: [],
 }
